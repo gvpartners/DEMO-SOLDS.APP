b493551528f087c714f9766888eab769ea7369d1
sebstianrodrigo <s.gvrodrigo@gmail.com>
2023-12-03 23:07:52 -0500

    updte program

--- DEMO-SOLDS.APP/Program.cs
+++ DEMO-SOLDS.APP/Program.cs
@@ -51,11 +51,12 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+    
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors(corsRules);
 
 app.UseHttpsRedirection();
